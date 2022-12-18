0f30c14f969e0135ad70f1823f0cffb8e7d64e2e
Egor Pristavka <pristavkaegor03@gmail.com>
2022-12-18 21:32:32 +0800

    Version 1.3 Commit
    - Puzzle game works stable
    - Sounds Added
    - 3 Difficulty levels
    - Auto completion

--- scripts/game.js
+++ scripts/game.js
@@ -1,2 +1,146 @@
-console.log(window.localStorage.getItem('puzzle_image'))
-console.log(window.localStorage.getItem('difficulty'))
+let puzzleImageLink = '';
+let imageB64 = '';
+let difficulty = null;
+let globalPuzzle = null;
+
+const WIN_SOUND = "./sounds/you_won.wav";
+const CLICK_SOUND = "./sounds/click.wav";
+const GIVE_UP_SOUND = "./sounds/give_up.wav"
+
+let difficultyLevels = {
+    "easy_mode": "Easy",
+    "medium_mode": "Medium",
+    "hard_mode": "Hard"
+}
+
+
+/**Builds custom alert with SweetAlert */
+function buildCustomAlert(title, text) {
+    const wrapper = document.createElement('div');
+    swal({
+        title: title,
+        text: text,
+        content: wrapper
+    });
+}
+
+
+/**Plays audiofile*/
+function play_sound(sound_path){
+    var audio = new Audio(sound_path);
+    audio.play();
+}
+
+
+function giveUp(){
+    play_sound(CLICK_SOUND);
+    play_sound(GIVE_UP_SOUND);
+    buildCustomAlert("What a pity!", "Look, who's given up here? Okay, we will collect the puzzle for you, so be it. But this is the last time.")
+    if(globalPuzzle != null){
+        globalPuzzle.solve();
+        globalPuzzle.redraw();
+    }
+}
+
+
+/**Redirects to settings */
+function changeData(){
+    play_sound(CLICK_SOUND);
+    this.location.href = "index.html";
+}
+
+
+
+
+/**Gets required data for game start */
+function getPuzzleInitData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    puzzleImageLink = urlParams.get('image');
+    difficulty = difficultyLevels[urlParams.get('difficulty')]
+    const difficulty_header = document.getElementById('difficulty_header')
+
+    if (difficulty != null) {
+        difficulty_header.innerHTML = `DIFFICULTY: ${difficulty}`
+    }
+
+    else {
+        difficulty_header.innerHTML = `DIFFICULTY: INSANE`
+        difficulty = 'Insane'
+        puzzleImageLink = 'https://i.ibb.co/7GMds31/image.png'
+    }
+    
+    const sample_canvas = document.getElementById('tvshape')
+    sample_canvas.style.backgroundSize = 'cover';
+    sample_canvas.style.backgroundImage = 'url(' + puzzleImageLink + ')';
+    buildCustomAlert("Welcome!", "Welcome to the puzzle game! Now we split selected image to puzzle tiles, so you only need to wait a bit.")
+    processPuzzleGame()
+}
+
+/**Starts the jigsaw puzzle game */
+function processPuzzleGame() {
+    let cols = 0;
+    let rows = 0;
+    let tile_width = 0;
+    let tile_height = 0;
+    switch (difficulty) {
+        case 'Easy':
+            cols = 5;
+            rows = 3;
+            tile_width = 70;
+            tile_height = 90;
+            break;
+        case 'Medium':
+            cols = 8;
+            rows = 5;
+            tile_height = 50;
+            tile_width = 50;
+            break;
+        case 'Hard':
+            cols = 17;
+            rows = 11;
+            tile_height = 25
+            tile_width = 25;
+            break;
+    }
+
+    let loaded_image = new Image();
+    loaded_image.src = puzzleImageLink;
+    loaded_image.onload = () => {
+        const validated = new headbreaker.Canvas('puzzle-place', {
+            width: 900, height: 600,
+            pieceSize: tile_width, proximity: 20,
+            borderFill: 8, strokeWidth: 1.1,
+            outline: new headbreaker.outline.Rounded(),
+            lineSoftness: 0.18, image: loaded_image,
+            fixed: true
+        });
+        validated.autogenerate({
+            horizontalPiecesCount: cols,
+            verticalPiecesCount: rows
+        });
+        if (!validated.puzzle.pieces.length){
+            difficulty = 'Hard';
+            puzzleImageLink = 'https://i.ibb.co/7GMds31/image.png'
+            processPuzzleGame()
+        }
+   
+        validated.shuffle();
+        console.log(typeof(validated))
+        validated.draw();
+        validated.attachSolvedValidator();
+        var requestedCanvas =  document.getElementsByTagName('canvas')[0];
+        requestedCanvas.style.backgroundColor = '#dcded9';
+        globalPuzzle = validated;
+        validated.onValid(() => {
+            setTimeout(() => {
+                play_sound(WIN_SOUND)    
+                buildCustomAlert("Congratulations!", "You have successfully completed the selected puzzle! Great job! But it could have been done sooner.)")
+            }, 1500);
+        })
+
+    }
+
+}
+
+
+getPuzzleInitData()
--- scripts/main_menu.js
+++ scripts/main_menu.js
@@ -9,10 +9,8 @@ inputElement.addEventListener("change", processImage, false);
 /**ImgBB API key */
 let API_KEY = 'USE YOUR OWN!'
 
+/**URL for request */
 let REQUEST_URL = 'https://api.imgbb.com/1/upload'
-
-/**Delay function */
-const delay = ms => new Promise(res => setTimeout(res, ms));
 
 
 /**Resizes image by it's Base64 */
@@ -35,52 +33,48 @@ function resizedataB64(datas, wantedWidth, wantedHeight) {
     })
 }
 
+
+/**Plays audiofile*/
+function play_sound(sound_path){
+    var audio = new Audio(sound_path);
+    audio.play();
+}
+
+
+
 /**Converts Base64 back to file */
 function dataURLtoFile(dataurl, filename) {
     var arr = dataurl.split(','),
         mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), 
-        n = bstr.length, 
+        bstr = atob(arr[1]),
+        n = bstr.length,
         u8arr = new Uint8Array(n);
-        
-    while(n--){
+
+    while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
-    
-    return new File([u8arr], filename, {type:mime});
-}
 
-/**Generates random name*/
-function randomFilename(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+    return new File([u8arr], filename, { type: mime });
 }
 
 
 /**Sends request to ImgBB */
-function sendImgbbRequest(base64){
+function sendImgbbRequest(base64) {
     let body = new FormData()
     body.set('key', API_KEY)
     body.append('image', dataURLtoFile(base64, 'image.png'))
-    let name  = randomFilename(20)+".png"
-    console.log(name)
     return axios({
         method: 'POST',
-        name: name,
         url: 'https://api.imgbb.com/1/upload',
         data: body
-      })
+    })
 }
 
 
 
 /**Process uploaded image file */
 function processImage() {
+    play_sound("./sounds/click.wav")
     let fileList = inputElement.files;
     let requested_file = fileList[0];
     if (FileReader && fileList && fileList.length) {
@@ -100,30 +94,38 @@ function processImage() {
 }
 
 
-
-function buildCustomAlert(title, text){
+/**Builds custom alert with SweetAlert */
+function buildCustomAlert(title, text) {
     const wrapper = document.createElement('div');
-        swal({
-            title: title,
-            text: text,
-            content: wrapper
-        });
+    swal({
+        title: title,
+        text: text,
+        content: wrapper
+    });
 }
 
 
 /**Start game onClick processing */
 function start_game() {
-    if (!allow_start) buildCustomAlert('Image not uploaded!', 'You should upload some image to start. Or are you going to play with nothing?)' )
+    if (!allow_start) buildCustomAlert('Image not uploaded!', 'You should upload some image to start. Or are you going to play with nothing?)')
     else {
-        let difficulty = document.querySelectorAll('input[name="answer-dark"]')[0].id
-        buildCustomAlert('Sending Data', 'We send your image to ImgBB server. So, you need a bit time to wait')
-        sendImgbbRequest(imgb64).then((response) => {
-            let viewUrl = response.data.data.display_url;
-            console.log(viewUrl)
-            buildCustomAlert("Successful!", "You will be redirected to the Game Page right now!")
-            this.location.href = `puzzle.html?image=${viewUrl}&difficulty=${difficulty}`;
-            
-        }).catch(error => buildCustomAlert("ERROR!", "Connection to ImgBB is unavailable! Sorry, we can't run puzzle game then ("));
+        if (API_KEY == 'USE YOUR OWN!') {
+            buildCustomAlert("YOU FOOL!", "Why are you trying to invoke ImgBB's API without the API KEY? Yes, you must use your own, OMG!. So go to the /scripts/main_menu.js and set variable API_KEY's value to ur key. And don't repeat such mistakes anymore!")
+            play_sound("./sounds/give_up.wav")
+        }
+        else {
+            play_sound("./sounds/click.wav")
+            console.log(document.querySelectorAll('input[name="answer-dark"]:checked')[0].id)
+            let difficulty = document.querySelectorAll('input[name="answer-dark"]:checked')[0].id
+            buildCustomAlert('Sending Data', 'We send your image to ImgBB server. So, you need a bit time to wait')
+            sendImgbbRequest(imgb64).then((response) => {
+                let viewUrl = response.data.data.display_url;
+                console.log(difficulty)
+                buildCustomAlert("Successful!", "You will be redirected to the Game Page right now!")
+                this.location.href = `puzzle.html?image=${viewUrl}&difficulty=${difficulty}`;
+
+            }).catch(error => buildCustomAlert("ERROR!", "Connection to ImgBB is unavailable! Sorry, we can't run puzzle game then ("));
+        }
 
     }
 }
